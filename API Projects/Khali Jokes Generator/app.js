bc0de0f5b17e54a58aaa17bb37eb29b584cc29be
Rishabh Bhardwaj <58872311+rishabh1815769@users.noreply.github.com>
2021-06-07 21:34:13 +0530

    Add files via upload

---
+++ API Projects/Khali Jokes Generator/app.js
@@ -0,0 +1,37 @@
+document.querySelector('#get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET',`http://api.icndb.com/jokes/random/${number}?firstName=The Great&lastName=Khali`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+
+      let output = '';
+
+      if(response.type === 'success') {
+
+        response.value.forEach(function(joke){
+          output += `<li class="list-group-item">${joke.joke}</li>`;
+        });
+
+
+
+      }else {
+        output += '<li class="list-group-item">Something went wrong</li>';
+      }
+
+      document.querySelector('#jokes').innerHTML = output;
+
+
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
